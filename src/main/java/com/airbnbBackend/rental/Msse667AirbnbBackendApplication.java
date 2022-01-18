1944a8265e0a0f71710f43eccfb1b163de65e36b
dcruz354 <dcruz354@gmail.com>
2022-01-18 13:34:28 -0700

    first commit

---
+++ src/main/java/com/airbnbBackend/rental/Msse667AirbnbBackendApplication.java
@@ -0,0 +1,13 @@
+package com.airbnbBackend.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Msse667AirbnbBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Msse667AirbnbBackendApplication.class, args);
+	}
+
+}
